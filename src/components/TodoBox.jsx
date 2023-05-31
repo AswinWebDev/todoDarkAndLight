a312e6733058d3560bcff9fd726f78a37e429229
AswinWebDev <aswinvinod0014@gmail.com>
2023-05-31 11:00:12 +0530

    responsiveness added

--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -3,52 +3,60 @@ import NewTodo from "./NewTodo";
 import CreatedTodo from "./CreatedTodo";
 import TodoLogo from "./TodoLogo";
 import Darkmode from "./Darkmode";
-import {DarkGrayishBlue ,VeryDarkGrayishBlueDark26} from "../colors";
+import { DarkGrayishBlue, VeryDarkGrayishBlueDark26 } from "../colors";
 const Container = styled.div`
-position: absolute;
-left: 32.5%;
-top: 25%;
-margin-right:auto;
-margin-left:auto;
-max-width: 700px;
-@media (max-width: 1200px) {
-  left: 25%;
-}
-@media (max-width: 1000px) {
-  left: 20%;
-}
-@media (max-width: 768px) {
-  left: 15%;
-}
-@media (max-width: 500px) {
-  top: 10%;
-  left: 10%;
-}
-  `;
+  position: absolute;
+  left: 50%;
+  top: 25%;
+  transform: translateX(-50%);
+  max-width: 700px;
+  @media (max-width: 1200px) {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  @media (max-width: 1000px) {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  @media (max-width: 768px) {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  @media (max-width: 500px) {
+    top: 10%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
+
 const TodoLogoWithDarkMode = styled.div`
-display:flex;
-align-items:center;
-justify-content:space-between;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 const ReorderListText = styled.div`
-margin-top:40px;
-text-align:center;
-opacity:0.9;
-color:${props=>props.darkMode?DarkGrayishBlue:VeryDarkGrayishBlueDark26}; 
-margin-bottom:40px;`;
-const TodoBox = ({darkMode,setDarkMode}) => {
+  margin-top: 40px;
+  text-align: center;
+  opacity: 0.9;
+  color: ${(props) =>
+    props.darkMode ? DarkGrayishBlue : VeryDarkGrayishBlueDark26};
+  margin-bottom: 40px;
+`;
+const TodoBox = ({ darkMode, setDarkMode }) => {
   //   const NewTodo = styled.div``;
   //   const CreatedTodo = styled.div``;
 
   return (
     <Container>
       <TodoLogoWithDarkMode>
-       <TodoLogo />
-       <Darkmode darkMode={darkMode} setDarkMode={setDarkMode}/>
+        <TodoLogo />
+        <Darkmode darkMode={darkMode} setDarkMode={setDarkMode} />
       </TodoLogoWithDarkMode>
-      <NewTodo darkMode={darkMode}/>
-      <CreatedTodo darkMode={darkMode}/>
-      <ReorderListText darkMode={darkMode}>Drag and drop to reorder list</ReorderListText>
+      <NewTodo darkMode={darkMode} />
+      <CreatedTodo darkMode={darkMode} />
+      <ReorderListText darkMode={darkMode}>
+        Drag and drop to reorder list
+      </ReorderListText>
     </Container>
   );
 };
